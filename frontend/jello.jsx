61204d4a82b629fb18b746c8928273382361548a
lordrickyz <rickyzheng@ymail.com>
2020-06-01 22:11:05 -0400

    Added Frontend in components

--- frontend/jello.jsx
+++ frontend/jello.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Jello > In FrontAuth Branch</h1>, root);
 });

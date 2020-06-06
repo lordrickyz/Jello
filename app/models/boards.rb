c1c3ed706682caa95dfc68d88b15cfc34e30e142
lordrickyz <rickyzheng@ymail.com>
2020-06-06 11:41:25 -0400

    Added validations for board model

---
+++ app/models/boards.rb
@@ -0,0 +1,15 @@
+class Board < ApplicationRecord
+  validates :title, :admin_id, presence: true
+  before_validation :assign_default_label
+
+  def assign_default_label
+    return if (self.title?)
+
+    self.title = "Default Board Title"
+  end
+
+  def is_admin?(user)
+    user.id == self.admin_id
+  end
+
+end
